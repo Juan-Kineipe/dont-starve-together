6c563b9ea21f4ac9407755d5ba6e8fd672589a26
Juan Kineipe <juan.kineipe@gmail.com>
2024-01-11 20:29:44 -0300

    feat: add exception handlers

---
+++ src/main/java/com/example/dontstarvetogetherapi/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.dontstarvetogetherapi.exception.handler;
+
+import com.example.dontstarvetogetherapi.exception.ExceptionResponse;
+import com.example.dontstarvetogetherapi.exception.InvalidNameException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception e, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(InvalidNameException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception e, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+}
