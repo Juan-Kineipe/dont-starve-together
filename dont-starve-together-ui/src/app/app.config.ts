e092ed8873f3ee5b5f8fac1dc79f5627ffcc4ec0
Juan Kineipe <juan.kineipe@gmail.com>
2024-01-15 20:09:29 -0300

    feat: add loader component

--- dont-starve-together-ui/src/app/app.config.ts
+++ dont-starve-together-ui/src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
 
 import { routes } from "./app.routes";
 
+import { NgxSpinnerModule } from "ngx-spinner";
+import { provideAnimations } from "@angular/platform-browser/animations";
+
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideHttpClient()],
+    providers: [
+        provideRouter(routes),
+        provideHttpClient(),
+        importProvidersFrom(NgxSpinnerModule.forRoot()),
+        provideAnimations(),
+    ],
 };
