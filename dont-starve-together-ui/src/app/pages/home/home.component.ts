849018b463680e0994023e588b191f114a5398b5
Juan Kineipe <juan.kineipe@gmail.com>
2024-01-15 20:45:39 -0300

    feat: add home page

--- dont-starve-together-ui/src/app/pages/home/home.component.ts
+++ dont-starve-together-ui/src/app/pages/home/home.component.ts
@@ -1,25 +1,44 @@
-import { Component, Inject, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { NgxSpinnerModule, NgxSpinnerService } from "ngx-spinner";
 import { SurvivorService } from "../../services/survivor.service";
 import { Survivor } from "../../models/survivor";
 
 @Component({
     selector: "app-home",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, NgxSpinnerModule],
     templateUrl: "./home.component.html",
     styleUrl: "./home.component.scss",
 })
 export class HomeComponent implements OnInit {
     survivorList: Survivor[] = [];
+    activeSurvivor: Survivor | undefined;
 
-    constructor(private survivorService: SurvivorService) {}
+    constructor(
+        private spinner: NgxSpinnerService,
+        private survivorService: SurvivorService
+    ) {}
 
     ngOnInit(): void {
-        this.survivorService.getAllSurvivors().subscribe((res) => {
-            if (res) {
+        this.getAllSurvivors();
+    }
+
+    getAllSurvivors() {
+        this.spinner.show();
+        this.survivorService.getAllSurvivors().subscribe(
+            (res) => {
                 this.survivorList = res;
+                this.activeSurvivor = res[0];
+                this.spinner.hide();
+            },
+            () => {
+                this.spinner.hide();
             }
-        });
+        );
+    }
+
+    showSurvivorInfo(survivor: Survivor) {
+        this.activeSurvivor = survivor;
     }
 }
