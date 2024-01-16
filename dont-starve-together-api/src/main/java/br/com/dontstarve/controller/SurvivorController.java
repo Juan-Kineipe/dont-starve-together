a64c52bb491e975107940a2c8ff1488f171a43f2
Juan Kineipe <juan.kineipe@gmail.com>
2024-01-16 20:05:11 -0300

    feat: add custom swagger documentation

--- dont-starve-together-api/src/main/java/br/com/dontstarve/controller/SurvivorController.java
+++ dont-starve-together-api/src/main/java/br/com/dontstarve/controller/SurvivorController.java
@@ -1,7 +1,11 @@
-package com.example.dontstarvetogetherapi.controller;
+package br.com.dontstarve.controller;
 
-import com.example.dontstarvetogetherapi.domain.Survivor;
-import com.example.dontstarvetogetherapi.service.SurvivorService;
+import br.com.dontstarve.domain.Survivor;
+import br.com.dontstarve.service.SurvivorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +14,24 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/survivors")
+@Tag(name = "Survivor", description = "Endpoints to get Survivor related information")
 public class SurvivorController {
 
     @Autowired
     private SurvivorService survivorService;
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Finds all Survivors", description = "Finds all Survivors",
+            tags = {"Survivor"}
+    )
     public List<Survivor> getAllSurvivors() {
         return survivorService.getAllSurvivors();
     }
 
     @RequestMapping(value="/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Finds a Survivor", description = "Finds a survivor",
+            tags = {"Survivor"}
+    )
     public Survivor getSurvivorByName(@PathVariable(value = "name") String name) {
         return survivorService.getSurvivorByName(name);
     }
