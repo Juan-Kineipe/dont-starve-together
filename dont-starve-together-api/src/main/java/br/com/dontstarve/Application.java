4ddac166f54090063a5e0d1dee93d098f8b8ab5d
Juan Kineipe <juan.kineipe@gmail.com>
2024-01-17 19:37:04 -0300

    configure build for railway

--- dont-starve-together-api/src/main/java/br/com/dontstarve/Application.java
+++ dont-starve-together-api/src/main/java/br/com/dontstarve/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    public String PORT = System.getenv("PORT");
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
