87ab0c9c084204b4e516d35ee15cdecf0ce540ca
Juan Kineipe <juan.kineipe@gmail.com>
2024-02-01 22:03:24 -0300

    feat: add mock server

--- dont-starve-together-ui/src/app/app.config.ts
+++ dont-starve-together-ui/src/app/app.config.ts
@@ -1,16 +1,17 @@
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { provideRouter } from "@angular/router";
-import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
 
 import { routes } from "./app.routes";
 
 import { NgxSpinnerModule } from "ngx-spinner";
 import { provideAnimations } from "@angular/platform-browser/animations";
+import { apiInterceptor } from "./shared/interceptors/api,interceptor";
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
-        provideHttpClient(),
+        provideHttpClient(withInterceptors([apiInterceptor])),
         importProvidersFrom(NgxSpinnerModule.forRoot()),
         provideAnimations(),
     ],
--- dont-starve-together-ui/src/environments/environment.ts
+++ dont-starve-together-ui/src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
     apiUrl: "https://dont-starve-together-production.up.railway.app",
+    mockUrl: "https://12cb912d-b361-42a2-8e02-daf6220a7a8b.mock.pstmn.io",
+    useMock: true,
 };
